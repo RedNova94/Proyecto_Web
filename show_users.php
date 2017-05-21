a522ae6a0fed2c39c1f61813af8fcc759b575328
BeardedYapper <suheilypergola@gmail.com>
2017-05-21 12:36:40 -0500

    Update show_users.php

--- show_users.php
+++ show_users.php
@@ -5,3 +5,37 @@
  * Date: 21/05/2017
  * Time: 12:18 PM
  */
+$PageTitle="Users";
+include_once ("header.php");
+
+include "database/database_connection.php";
+$sql_query = "SELECT * FROM users";
+$resultado = mysqli_query($conexion, $sql_query);
+
+while ($row = mysqli_fetch_array($resultado)){ ?>
+    <div class="box cd">
+        <table>
+            <tr>
+                <td><strong> Nombre </strong></td>
+                <td> <?php echo $row['username'] ?> </td>
+            </tr>
+            <tr>
+                <td><strong> Password </strong></td>
+                <td> <?php echo $row['password'] ?>
+            </tr>
+            <tr>
+                <td><strong> Email </strong></td>
+                <td> <?php echo $row['email'] ?> </td>
+            </tr>
+            <tr>
+                <td><strong> Admin </strong></td>
+                <td> <?php echo $row['admin'] ?> </td>
+            </tr>
+
+        </table>
+    </div>
+
+    <br>
+    <br>
+
+<?php } ?>

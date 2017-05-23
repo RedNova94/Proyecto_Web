560971a282729bbc2eec5ff885b19db376e3f29a
Alberto Vasquez <rnvasquez94@gmail.com>
2017-05-23 17:41:17 -0500

    la imagen no se borra si falla el envío

--- upload.php
+++ upload.php
@@ -26,61 +26,59 @@ if(isset($_POST["submit"])) {
     } else {
         echo "<script>
                 alert('File is not an image.');
-                window.location='upload_avatar.php';
+                window.location='user_profile.php';
               </script>";
         $uploadOk = 0;
     }
 }
-// Check if file already exists
-if (file_exists($tmp_name)) {
-/*    echo "<script>
-                alert('File already exists');
-                window.location='upload_avatar.php';
-              </script>";*/
-    if ($handle = opendir('profile_pictures/')) {
-        if (file_exists('profile_pictures/'.$name.'.png')) {
-            unlink('profile_pictures/'.$name.'.png');
-        } else if (file_exists('profile_pictures/'.$name.'.jpg')) {
-            unlink('profile_pictures/'.$name.'.jpg');
-        }
-    }
-    closedir($handle);
-    $uploadOk = 1;
-}
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "<script>
-                alert('Sorry, your file is too large.');
-                window.location='upload_avatar.php';
+                alert('La imagen es muy grande.');
+                window.location='user_profile.php';
               </script>";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageExt != "jpg" && $imageExt != "png") {
     echo "<script>
-                alert('Sorry, only JPG and PNG files are allowed.');
-                window.location='upload_avatar.php';
+                alert('Solo se permiten .jpg y .png');
+                window.location='user_profile.php';
               </script>";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {;
     echo "<script>
-                alert('Sorry, your file was not uploaded.');
-                window.location='upload_avatar.php';
+                alert('Hubo un error al subir la imagen.');
+                window.location='user_profile.php';
               </script>";
 // if everything is ok, try to upload file
 } else {
+    if (file_exists($tmp_name)) {
+        /*    echo "<script>
+                        alert('File already exists');
+                        window.location='upload_avatar.php';
+                      </script>";*/
+        if ($handle = opendir('profile_pictures/')) {
+            if (file_exists('profile_pictures/'.$name.'.png')) {
+                unlink('profile_pictures/'.$name.'.png');
+            } else if (file_exists('profile_pictures/'.$name.'.jpg')) {
+                unlink('profile_pictures/'.$name.'.jpg');
+            }
+        }
+        closedir($handle);
+    }
     if (move_uploaded_file($tmp_name, $target_dir.$full_name)) {
         //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         echo "<script>
         alert('Imagen Guardada');
         window.location='user_profile.php';
-      </script>";
+            </script>";
     } else {
         echo "<script>
-                alert('There was an error uploading your files');
-                window.location='upload_avatar.php';
+                alert('Hubo un error al subir la imagen.');
+                window.location='user_profile.php';
               </script>";
     }
 }

1cc7518ee56d556397ff39db2f2fc9428fe55f37
Alberto Vasquez <rnvasquez94@gmail.com>
2017-05-22 22:50:34 -0500

    Cambia tu imagen de perfil, pero solo una vez

--- upload.php
+++ upload.php
@@ -9,7 +9,7 @@ session_start();
 $target_dir = "profile_pictures/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
 
 
 
@@ -18,6 +18,10 @@ if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         //echo "File is an image - " . $check["mime"] . ".";
+        $name = $_SESSION['shadowplay_username'];
+        $imageExt = pathinfo($target_file,PATHINFO_EXTENSION);
+        $tmp_name = $_FILES["fileToUpload"]["tmp_name"];
+        $full_name = $name.'.'.$imageExt;
         $uploadOk = 1;
     } else {
         echo "<script> 
@@ -41,7 +45,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png") {
+if($imageExt != "jpg" && $imageExt != "png") {
     echo "<script> 
                 alert('Sorry, only JPG and PNG files are allowed.');
                 window.location='upload_avatar.php';
@@ -56,7 +60,7 @@ if ($uploadOk == 0) {;
               </script>";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($tmp_name, $target_dir.$full_name)) {
         //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         echo "<script> 
         alert('Imagen Guardada');
